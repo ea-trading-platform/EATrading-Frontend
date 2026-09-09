f2f0f39d2104da0404d22e9224e9e9e6379dcbc9
Angelos Boules <angelos.boules24@gmail.com>
2026-09-09 14:45:50 +0000

    feat: Build account balance graph component

---
+++ frontend/src/app/features/dashboard/components/balance-graph/balance-graph.ts
@@ -0,0 +1,56 @@
+import { Component, computed, input } from '@angular/core';
+import { AgCharts } from "ag-charts-angular";
+import {
+  AgChartOptions,
+  LegendModule,
+  LineSeriesModule,
+  ModuleRegistry,
+  NumberAxisModule,
+  UnitTimeAxisModule,
+} from "ag-charts-community";
+
+ModuleRegistry.registerModules([
+  LegendModule,
+  LineSeriesModule,
+  NumberAxisModule,
+  UnitTimeAxisModule,
+]);
+
+@Component({
+  selector: 'app-balance-graph',
+  standalone: true,
+  imports: [AgCharts],
+  styleUrl: './balance-graph.css',
+  templateUrl: './balance-graph.html',
+})
+export class BalanceGraph {
+  graphData = input<any[]>([]);
+
+  options = computed(() => ({
+    title: {
+      text: "Total Account Balance",
+    },
+    series: [
+      {
+        type: "line",
+        data: this.graphData,
+        xKey: "date",
+        yKey: "balance",
+        yName: "Account Balance",
+      },
+    ],
+    axes: {
+      x: {
+        type: "unit-time",
+      },
+      y: {
+        type: "number",
+        label: {
+          format: "#${0.2f}",
+        },
+      },
+    },
+  }));
+
+
+}
